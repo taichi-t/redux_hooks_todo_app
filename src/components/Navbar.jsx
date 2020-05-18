f246393774c80adeeef9f20a8eb64cbbf25308bb
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-05-18 14:23:05 -0700

    add:date on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import DarkModeToggle from "react-dark-mode-toggle";
 import { useTheme } from "@material-ui/core/styles";
+import moment from "moment";
 
 //style
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 
 export const Navbar = (props) => {
+  //state
   const theme = useTheme();
-
   const [isDarkMode, setIsDarkMode] = useState(() => false);
   const { toggleDarkMode } = props;
+  const [now, setNow] = useState("");
 
+  useEffect(() => {
+    const setTime = () => {
+      const month = moment().format("MMMM");
+      const date = moment().format("Do");
+      setNow({ month, date });
+    };
+    setTime();
+    setInterval(setTime, 60000);
+  }, []);
+
+  //handle actions
   const handleClick = (e) => {
     e.preventDefault();
     setIsDarkMode(!isDarkMode);
@@ -23,7 +36,10 @@ export const Navbar = (props) => {
     <AppBar position="static">
       <Toolbar variant="dense">
         <Box>
-          <Title color={theme.palette.text.primary}>Awesome Todo list</Title>
+          <Title color={theme.palette.text.primary}>
+            <span>{now.date} </span>
+            <Month>{now.month}</Month>
+          </Title>
 
           <div onClick={handleClick}>
             <DarkModeToggle checked={isDarkMode} size={50} speed={6} />
@@ -48,4 +64,8 @@ const Box = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+`;
+
+const Month = styled.span`
+  font-size: 1rem;
 `;
