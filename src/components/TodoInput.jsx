1948eddc162b54c8bf4d09ba7ab526c25044131f
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-05-17 20:28:38 -0700

    create:execute button

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -11,6 +11,7 @@ import styled from "styled-components";
 import AddIcon from "@material-ui/icons/Add";
 import Paper from "@material-ui/core/Paper";
 import DoneAllIcon from "@material-ui/icons/DoneAll";
+import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
 
 export const TodoInput = () => {
   const [todo, setTodo] = useState("");
@@ -47,25 +48,31 @@ export const TodoInput = () => {
           autoComplete="off"
           style={{ padding: "1rem" }}
         >
-          <Box>
-            <Input
-              type="text"
-              name="todo"
-              placeholder="create a todo"
-              value={todo}
-              onChange={handleChange}
-            />
-            <Button type="submit" color="primary" startIcon={<AddIcon />}>
-              ADD
-            </Button>
-            <Button
-              onClick={handleClick}
-              color="primary"
-              startIcon={<DoneAllIcon />}
-            >
-              <Text fontSize="0.5rem">Select</Text>
-            </Button>
-          </Box>
+          <Input
+            type="text"
+            name="todo"
+            placeholder="create a todo"
+            value={todo}
+            onChange={handleChange}
+          />
+          <Button type="submit" color="primary" startIcon={<AddIcon />}>
+            ADD
+          </Button>
+
+          <StyledButton
+            onClick={handleClick}
+            color="primary"
+            startIcon={<DoneAllIcon />}
+          >
+            <Text fontSize={0.5}>Select All</Text>
+          </StyledButton>
+          <StyledButton
+            onClick={handleClick}
+            color="primary"
+            startIcon={<CheckCircleOutlineIcon />}
+          >
+            <Text fontSize={0.5}>Execute</Text>
+          </StyledButton>
         </form>
       </Paper>
     </Container>
@@ -83,11 +90,10 @@ const Container = styled.div`
   margin: 0 auto;
 `;
 
-const Box = styled.div`
-  display: flex;
-  align-items: center;
+const Text = styled.p`
+  font-size: ${(props) => props.fontSize}rem;
 `;
 
-const Text = styled.p`
-  font-size: ${(props) => props.fontSize};
+const StyledButton = styled(Button)`
+  margin-right: 1rem;
 `;
