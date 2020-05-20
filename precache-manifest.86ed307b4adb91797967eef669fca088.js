c8778bf2a321b25c9e0f049c13fee0205ae1f513
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-05-20 03:34:50 -0700

    Updates

---
+++ precache-manifest.86ed307b4adb91797967eef669fca088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb26a35bddc0ba472214721c8f38281",
+    "url": "/deploy_redux_hooks_app/index.html"
+  },
+  {
+    "revision": "37b6281a61e0331c0d8a",
+    "url": "/deploy_redux_hooks_app/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "37b6281a61e0331c0d8a",
+    "url": "/deploy_redux_hooks_app/static/js/2.78b6d3a1.chunk.js"
+  },
+  {
+    "revision": "6fd04ba363d572dd03a3571282a1ff49",
+    "url": "/deploy_redux_hooks_app/static/js/2.78b6d3a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "441d650cad196752f2d5",
+    "url": "/deploy_redux_hooks_app/static/js/main.d9bba52b.chunk.js"
+  },
+  {
+    "revision": "d7ec0b2347e99eccdaaa",
+    "url": "/deploy_redux_hooks_app/static/js/runtime-main.0c667b7b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/deploy_redux_hooks_app/precache-manifest.1e64ad285c99318ba10753187ebeb217.js"
+  "/deploy_redux_hooks_app/precache-manifest.86ed307b4adb91797967eef669fca088.js"
 );
 
 self.addEventListener('message', (event) => {
