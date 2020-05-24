88dd3a416720031f6aaf6aafd33a924909e73ff5
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-05-24 04:29:34 -0700

    Updates

--- precache-manifest.3532c86760248a655d6d24353435f1f0.js
+++ precache-manifest.3532c86760248a655d6d24353435f1f0.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9907f5f5a85a5e5a27e0e28b75f1c83d",
+    "revision": "1329035f1ba6138691667377041e4689",
     "url": "/deploy_redux_hooks_app/index.html"
   },
   {
-    "revision": "eaba6e83261bcb36a3b1",
+    "revision": "c7849c4f1d713517de15",
     "url": "/deploy_redux_hooks_app/static/css/2.764ccc25.chunk.css"
   },
   {
-    "revision": "eaba6e83261bcb36a3b1",
-    "url": "/deploy_redux_hooks_app/static/js/2.62a9e8de.chunk.js"
+    "revision": "c7849c4f1d713517de15",
+    "url": "/deploy_redux_hooks_app/static/js/2.e1caa667.chunk.js"
   },
   {
     "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
-    "url": "/deploy_redux_hooks_app/static/js/2.62a9e8de.chunk.js.LICENSE.txt"
+    "url": "/deploy_redux_hooks_app/static/js/2.e1caa667.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "201cc0cfade223a3409c",
-    "url": "/deploy_redux_hooks_app/static/js/main.76e8c172.chunk.js"
+    "revision": "16aa901df7ede45b85cc",
+    "url": "/deploy_redux_hooks_app/static/js/main.f68b7fae.chunk.js"
   },
   {
     "revision": "d7ec0b2347e99eccdaaa",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/deploy_redux_hooks_app/precache-manifest.15956ebbde10c411f27050163dcb0308.js"
+  "/deploy_redux_hooks_app/precache-manifest.3532c86760248a655d6d24353435f1f0.js"
 );
 
 self.addEventListener('message', (event) => {
