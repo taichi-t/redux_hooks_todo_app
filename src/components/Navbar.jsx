450e84d005b98ef2a6fa492c71398259b0032d02
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-05-24 01:37:46 -0700

    modify: navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ import IconButton from "@material-ui/core/IconButton";
 
 const useStyles = makeStyles((theme) => ({
   menuButton: {
-    marginRight: theme.spacing(3),
+    marginRight: theme.spacing(2),
   },
   title: {
     alignSelf: "flex-end",
