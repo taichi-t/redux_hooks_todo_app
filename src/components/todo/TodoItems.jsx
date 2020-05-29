67119b42f365bc356fcb3ecf8762ad380231c64e
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-05-28 23:39:37 -0700

    delete:unuse components

--- src/components/todo/TodoItems.jsx
+++ src/components/todo/TodoItems.jsx
@@ -16,8 +16,6 @@ import Button from "@material-ui/core/Button";
 import DoneIcon from "@material-ui/icons/Done";
 import IconButton from "@material-ui/core/IconButton";
 import { makeStyles } from "@material-ui/core/styles";
-import BackspaceOutlinedIcon from "@material-ui/icons/BackspaceOutlined";
-import HighlightOffIcon from "@material-ui/icons/HighlightOff";
 import DeleteIcon from "@material-ui/icons/Delete";
 
 export const TodoItems = (props) => {
