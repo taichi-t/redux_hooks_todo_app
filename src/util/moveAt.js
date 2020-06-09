1a250eb9daae89fb0222c767a303b91ddd70950d
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-06-08 23:19:56 -0700

    delete:console.log

--- src/util/moveAt.js
+++ src/util/moveAt.js
@@ -1,13 +1,8 @@
 export const moveAt = (array, index, at) => {
-  console.log(array, index, at);
   const value = array[index];
   const tail = array.slice(index + 1);
-
   array.splice(index);
-
   Array.prototype.push.apply(array, tail);
-
   array.splice(at, 0, value);
-
   return array;
 };
