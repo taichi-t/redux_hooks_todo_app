52c42430a4d56cede63163af95d698efa24e9832
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-12-08 19:14:01 -0800

    change: home page namewq

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,22 +1,23 @@
-import React from "react";
-import "normalize.css";
+import React from 'react';
+import 'normalize.css';
 
 /* ------------------------------- COMPONENTS ------------------------------- */
-import { Home } from "../home/Home";
+import { Home } from '../home/Home';
 
 /* ------------------------------- CONTEXT Provider ------------------------------ */
-import { Provider as UiProvider } from "../../store/context/provider";
+import { Provider as UiProvider } from '../../store/context/provider';
 
 /* ---------------------------------- HOOKS --------------------------------- */
-import { useTheme } from "../../hooks/useTheme";
+import { useTheme } from '../../hooks/useTheme';
 
 /* ---------------------------------- STYLE --------------------------------- */
-import { GlobalStyle } from "../../GlobalStyle";
-import { ThemeProvider } from "@material-ui/core/styles";
-import { createMuiTheme } from "@material-ui/core/styles";
+import { GlobalStyle } from '../../GlobalStyle';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
 
 export const App = () => {
   const [theme] = useTheme();
+
   const themeConfig = createMuiTheme(theme);
   return (
     <UiProvider>
